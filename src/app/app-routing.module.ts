8bf7a7279c1d67b4c2ab34b05be846a9b2703754
Martin Juul <code@juul.xyz>
2018-06-09 16:29:30 +0200

    [AppComponent]: Slimmed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 import { HomeComponent } from './components/home/home.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-  {
-    path: 'settings',
-    component: SettingsComponent
-  }
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
+  { path: 'auth', children: [
+      { path: 'signin', component: SigninComponent },
+      { path: 'signup', component: SignupComponent }
+    ] }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: true})],
-    exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes, { useHash: true }) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule
+{
+}
